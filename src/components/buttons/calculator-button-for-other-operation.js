eb4024c34ab6302200bf32c6113407ddb55018ff
Aleksei Zhuchkov <kelzzzer@gmail.com>
2025-04-30 21:42:58 +0300

    fix: styles for small screen

--- src/components/buttons/calculator-button-for-other-operation.js
+++ src/components/buttons/calculator-button-for-other-operation.js
@@ -1,8 +1,14 @@
 import { calculatorButton } from './calculator-button';
 import styles from './calculator-button.module.scss';
 
-export const calculatorButtonForOtherOperation = (parent, value, command) => {
-  const button = calculatorButton(parent, value, command);
+export const calculatorButtonForOtherOperation = (
+  parent,
+  value,
+  command,
+  widthCell
+) => {
+  const button = calculatorButton(parent, value, command, widthCell);
   button.classList.add(styles.buttonForOtherOperation);
+  button.style.gridColumnEnd = `span ${widthCell}`;
   return button;
 };
