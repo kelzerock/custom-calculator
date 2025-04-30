5187a7c2d890be6edf0045cb103e8dc19c5e8dbc
Aleksei Zhuchkov <kelzzzer@gmail.com>
2025-04-30 22:44:07 +0300

    feat: separator to display number

--- src/display/display.js
+++ src/display/display.js
@@ -1,3 +1,7 @@
+import { addSeparator } from '../utils/add-separator';
+
+const MAX_LENGTH = 12;
+
 export class Display {
   display;
 
@@ -8,6 +12,11 @@ export class Display {
   }
 
   render() {
-    this.display.textContent = this.app.activeOperator.resultForView || 0;
+    const value = this.app.activeOperator.resultForView || '0';
+    const cutValue = value.slice(
+      0,
+      value.includes('.') ? MAX_LENGTH + 1 : MAX_LENGTH
+    );
+    this.display.textContent = addSeparator(cutValue);
   }
 }
---
+++ src/utils/add-separator.js
@@ -0,0 +1,36 @@
+const mainSeparator = (string) =>
+  string
+    .split('')
+    .reverse()
+    .map((el, i) =>
+      (i + 1) % 3 === 0 && i !== string.length - 1 ? `˙${el}` : el
+    )
+    .reverse()
+    .join('');
+
+const lastPartSeparator = (string) =>
+  string
+    .split('')
+    .map((el, i) =>
+      (i + 1) % 3 === 0 && i !== string.length - 1 ? `${el}˙` : el
+    )
+    .join('');
+
+export const addSeparator = (string) => {
+  if (
+    typeof string !== 'string' ||
+    Number.isNaN(Number(string.replace('-', '').replace('.', '')))
+  ) {
+    return 'Invalid input data!';
+  }
+  const isNegative = string.includes('-');
+  const cutString = isNegative ? string.slice(1) : string;
+  let result = '';
+  if (cutString.includes('.')) {
+    const [firstPart, secondPart] = cutString.split('.');
+    result = `${mainSeparator(firstPart)}.${lastPartSeparator(secondPart)}`;
+  } else {
+    result = mainSeparator(cutString);
+  }
+  return isNegative ? `-${result}` : result;
+};
